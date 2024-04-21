3b3d11c821858559873acb611fd5b246fb2e993f
merytis <rytis.merkininkas@gmail.com>
2024-04-21 19:13:17 +0300

    "Happy path" of game logic is covered. Progress is updated in ReadMe file.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # cardsWar
 Requirements for the program:
-1. There must be at least 2 object (classes) implemented 
-2. Inheritence should be used
-3. Program should ask enter name of players
-4. Program shoud print all the sequence of game in this format: 
++ 1. There must be at least 2 object (classes) implemented 
++ 2. Inheritence should be used
++ 3. Program should ask enter name of players
+- 4. Program shoud print all the sequence of game in this format: 
     Player1 card: 9heart
     Player2 card: 2heart
     Player1 takes cards
-5. Program must print the winner after game ends
-6. Main logic of code should be covered by unit tests
+- 5. Program must print the winner after game ends
+- 6. Main logic of code should be covered by unit tests
--- war.py
+++ war.py
@@ -1,5 +1,7 @@
-ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
-suits = ["Club", "Diamond", "Heart", "Spade"]
+from random import shuffle
+
+RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+SUITS = ["Club", "Diamond", "Heart", "Spade"]
 
 
 class Card:
@@ -7,13 +9,16 @@ class Card:
         self.suit = suit
         self.rank = rank
 
+    def __str__(self) -> str:
+        return f"{self.rank}-{self.suit}"
+
     @property
     def suit(self) -> str:
         return self._suit
 
     @suit.setter
     def suit(self, suit: str):
-        if suit not in suits:
+        if suit not in SUITS:
             raise ValueError("Invalid suit of card")
         self._suit = suit
 
@@ -23,9 +28,9 @@ class Card:
 
     @rank.setter
     def rank(self, rank: str):
-        if rank not in ranks:
+        if rank not in RANKS:
             raise ValueError("Invalid rank of card")
-        self._rank
+        self._rank = rank
 
     ...
 
@@ -51,11 +56,11 @@ class Player(Person):
         self.cards = cards
 
     @property
-    def cards(self) -> list[cards]:
+    def cards(self) -> list[Card]:
         return self._cards
 
     @cards.setter
-    def cards(self, cards: list[cards]):
+    def cards(self, cards: list[Card]):
         self._cards = cards
 
     ...
@@ -63,16 +68,69 @@ class Player(Person):
 
 def main():
     deck = get_card_deck()
-    ...
+
+    first_player = Player(input("Player1: "), deal_cards(deck, True))
+    # firstPlayer = Player("Player1", deal_cards(deck, True))
+
+    second_player = Player(input("Player2: "), deal_cards(deck, False))
+    # secondPlayer = Player("Player2", deal_cards(deck, False))
+
+    play_game(first_player, second_player)
 
 
-def get_card_deck() -> list[Card]: ...
+def get_card_deck() -> list[Card]:
+    deck = []
+
+    for suit in SUITS:
+        for rank in RANKS:
+            card = Card(suit, rank)
+            deck.append(card)
+
+    shuffle(deck)
+    return deck
 
 
-def play_game(firstPlayer: Player, secondPlayer: Player) -> Player: ...
+def deal_cards(deck: list[Card], odd: bool) -> list[Card]:
+    cards = []
+    i = 1
+
+    for card in deck:
+        if odd and i % 2 != 0:
+            cards.append(card)
+        elif not odd and i % 2 == 0:
+            cards.append(card)
+        i += 1
+
+    return cards
 
 
-def play_war(firstPlayer: Player, secondPlayer: Player) -> Player: ...
+def play_game(first_player: Player, second_player: Player) -> Player:
+    first_player_stash = []
+    second_player_stash = []
+    round = 1
+
+    while first_player.cards and second_player.cards:
+        first_player_card = first_player.cards.pop()
+        second_player_card = second_player.cards.pop()
+
+        if RANKS.index(first_player_card.rank) > RANKS.index(second_player_card.rank):
+            first_player_stash.extend([first_player_card, second_player_card])
+            print(
+                f"Round {round}:\n{first_player.name} card {first_player_card} is higher than {second_player.name} card {second_player_card}"
+            )
+        elif RANKS.index(first_player_card.rank) < RANKS.index(second_player_card.rank):
+            second_player_stash.extend([first_player_card, second_player_card])
+            print(
+                f"Round {round}:\n{second_player.name} card {second_player_card} is higher than {first_player.name} card {first_player_card}"
+            )
+
+        round += 1
+        print(
+            f"Result:\n{first_player.name} has {len(first_player.cards)} cards and {len(first_player_stash)} in stash/n{second_player.name} has {len(second_player.cards)} cards and {len(second_player_stash)} in stash"
+        )
+
+
+def play_war(first_player: Player, second_player: Player) -> Player: ...
 
 
 if __name__ == "__main__":
